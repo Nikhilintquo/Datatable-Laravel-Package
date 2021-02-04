18dca9a82f271350482bb8f75b2370be1aac4acc
unknown <nikhil.intquo@gmail.com>
2021-02-04 14:06:44 +0530

    first commit

--- resources/views/datatable.blade.php
+++ resources/views/datatable.blade.php
@@ -15,7 +15,7 @@
 <body>
     <div class="conatiner m-5 p-5">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         <table id="example" class="display" style="width:100%">
